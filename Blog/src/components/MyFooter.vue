3f5afd36965cb865d5c663e05e3fc221f7ccaf04
wuxiaohen <2750127294@qq.com>
2017-03-07 16:28:11 +0800

    blog
    一个基于vue的blog

---
+++ Blog/src/components/MyFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <div class="copyright">
+            &copy;lyb  lybaihh@163.com
+        </div>
+        <div class="login" @click="login">
+            站长登陆
+        </div>
+    </footer>
+</template>
+<script>
+export default{
+    methods:{
+        login(){
+            this.$router.go('/login')
+        }
+    }
+}
+</script>
+<style lang="sass">
+@import "../style/components/MyFooter.scss";
+</style>
